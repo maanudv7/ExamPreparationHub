3171a3fe8eae4741c3095d16fe6bfe890ca575da
Himanshu Sharma <himusharma777@gmail.com>
2020-08-31 13:22:00 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $gClient=new Google_Client();
 $gClient->setClientId("255280094683-abhsmq3albag7b0te4rjac8orkdbk630.apps.googleusercontent.com");
 $gClient->setClientSecret("S_Lafm65XtDARK2nVz8--YAy");
 $gClient->setApplicationName("Appsignin");
-$gClient->setRedirectUri("http://localhost/task/call.php");
+$gClient->setRedirectUri("http://localhost/ExamPreparationHub/call.php");
  $gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
 
 ?>
